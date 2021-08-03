86223f7126a7a6cd33cb2048a495b90b1bf4e389
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-03 15:56:39 +0500

    Fix the Bug

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -5,7 +5,7 @@ export default function Post({ posts }) {
       {posts.map((post) => {
         return (
           <div key={post.id}>
-            <Link href={`posts/{post.id}`} passHref>
+            <Link href={`posts/${post.id}`} passHref>
               <h2>
                 {post.id} {post.title}
               </h2>
