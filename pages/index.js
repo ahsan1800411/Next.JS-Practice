b5a90c07379046e31b8212ecfde9718606f8018d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-03 10:11:35 +0500

    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
 export default function Index() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <Link href='/product/2'>
+        <a>Product Details Page</a>
+      </Link>
+    </div>
+  );
 }
