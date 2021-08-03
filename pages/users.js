fe70a22ca35a479ad469da95d79ec6256bd114b5
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-03 11:59:23 +0500

    Static Generation with getStatic Props

---
+++ pages/users.js
@@ -0,0 +1,26 @@
+function UsersList({ users }) {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+export default UsersList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
